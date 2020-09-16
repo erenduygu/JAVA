b3dec4806ca015a99f3547d1c9fea92c4af03ed0
erenduygu <71392533+erenduygu@users.noreply.github.com>
2020-09-16 17:24:28 +0300

    Add files via upload

---
+++ assignments/day_2/Question7.java
@@ -0,0 +1,17 @@
+package assignments.day_2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print(" /\\ ");
+		System.out.println(" /\\ ");
+		System.out.print("/__\\");
+		System.out.println("/__\\\n");
+		System.out.println("I am from " + "\"Germany\".\n");
+		System.out.println("How is your day going on,'good' or 'bad'?");
+
+	}
+
+}
---
+++ assignments/day_2/Question8.java
@@ -0,0 +1,23 @@
+package assignments.day_2;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			
+		System.out.print("*********    ");		System.out.println("******");
+		System.out.print("**           ");		System.out.println("**    **");
+		System.out.print("**           ");		System.out.println("**      **");
+		System.out.print("**           ");		System.out.println("**       **");
+		System.out.print("*******      ");		System.out.println("**       **");
+		System.out.print("**           ");		System.out.println("**       **");
+		System.out.print("**           ");		System.out.println("**      **");
+		System.out.print("**           ");		System.out.println("**     **");
+		System.out.print("*********    ");		System.out.println("******");	
+		
+		
+		
+	}
+
+}
